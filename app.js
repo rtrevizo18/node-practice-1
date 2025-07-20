349033f36c754ac638cc808179d5354e0745582d
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-20 16:20:42 -0500

    feat: SSR w/ form example

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log("INCOMING REQUEST");
-  console.log(req.method, req.url);
+const app = express();
 
-  res.end("Success");
+app.use((req, res, next) => {
+  let body = "";
+  req.on("end", () => {
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    next();
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 });
 
-server.listen(5001);
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send("<h1>" + req.body.name + "</h1>");
+  }
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(5001);
